249e928c450cfa3a1b7d67ea2d4e6a65b7e3611b
escarlata1991 <zyg.diaz@gmail.com>
2015-01-16 21:59:37 -0300

    modificacion archivo prueba

--- prueba.php
+++ prueba.php
@@ -1,7 +1,7 @@
 <<<<<<< HEAD
 hola juan, como esta ???
 sabes la mina rica que te envie por fotos nunca la vi.. mina calieeennnnte compadre
-=======
+=======asdfjasdhgfjhasgdjfgasjfgjasdghfjasdgfkjaghsdkfgaskdjgasdjf
 
 
 holalalalalalallala soy juan
