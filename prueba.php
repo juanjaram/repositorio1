cb5265be48846467d084bc0c6001218a5df14ccc
escarlata1991 <zyg.diaz@gmail.com>
2015-01-16 21:53:26 -0300

    modificacion archivo prueba

--- prueba.php
+++ prueba.php
@@ -1,8 +1,2 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+hola juan, como esta ???
+sabes la mina rica que te envie por fotos nunca la vi.. mina calieeennnnte compadre
