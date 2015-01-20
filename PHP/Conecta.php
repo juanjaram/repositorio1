48fd27b0dd38f77106a3088fdfbd4a8bc1080c62
Your Name <you@example.com>
2015-01-20 17:36:54 -0300

    200115-1736

--- PHP/Conecta.php
+++ PHP/Conecta.php
@@ -1,8 +1,14 @@
 <?php 
 
 // Conectando, seleccionando la base de datos
-$link = mysql_connect('localhost', 'root', 'root') or mysql_connect('localhost', 'root', '')
-    or die('No se pudo conectar: ' . mysql_error());
+$link = mysql_connect('localhost', 'root', 'root');
+     //  or die('No se pudo conectar: ' . mysql_error());
+if (!$link) {
+   
+$link = mysql_connect('localhost', 'root', '')
+or die('No pudo conectarse: ' . mysql_error());
+}
+  
 //echo 'Connected successfully';
 mysql_select_db('osteochi_nueva') or die('No se pudo seleccionar la base de datos');
 
